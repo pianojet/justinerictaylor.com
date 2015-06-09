7b9e5c2467258e624eaed868e7102b6aa283003f
Justin Taylor <justin@adonnetwork.com>
2015-06-08 23:00:30 -0400

    updates for subdomains and root domain

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 var express = require('express');
-var connect = require('connect');
-var vhost = require('vhost');
 var subdomain = require('express-subdomain');
 var app1 = require('./led/app.js');
 var app2 = require('./bb8/app.js');
