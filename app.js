e1633a28d8e005e574bee1aef5ce61f0bccc738a
Justin Taylor <justin@adonnetwork.com>
2015-06-08 22:27:29 -0400

    ton of changes and added bb8 subdomain code

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var connect = require('connect');
+var vhost = require('vhost');
+var subdomain = require('express-subdomain');
+var app1 = require('./led/app.js');
+var app2 = require('./bb8/app.js');
 
 express()
-.use(express.vhost('led.justinerictaylor.com', require('led/app.js').app))
-//.use(express.vhost('sync.mysite.com', require('/path/to/sync').app))
+.use(subdomain('led', app1))
+.use(subdomain('bb8', app2))
 .listen(80)
