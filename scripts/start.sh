c78d9a72d252567bad52b8d1b05fee2468c4cf3a
root <pianojet@gmail.com>
2015-06-10 09:04:48 -0400

    fixed start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-APP1=/root/justinerictaylor.com/led
-APP1=/root/justinerictaylor.com/bb8
+BASEAPP=/root/justinerictaylor.com
 
-sudo -u root -H sh -c "nvm use v0.10.38"
+APP1=$BASEAPP/led
+APP2=$BASEAPP/bb8
 
-cd APP1
-node app.js &
+apache2ctl restart
 
-cd APP2
-node app.js &
+export NVM_DIR="/root/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+nvm use v0.10.38
+
+cd $APP1
+node app.js > $BASEAPP/led.log 2> $BASEAPP/led.log &
+
+cd $APP2
+node app.js > $BASEAPP/bb8.log 2> $BASEAPP/bb8.log &
